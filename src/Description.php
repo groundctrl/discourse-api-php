3651a0604cd8a6fa68c40f3329603d7eba7084dc
Michael Crumm <mike.crumm@groundctrl.com>
2014-11-20 08:45:02 -0800

    Adding Discourse Description

---
+++ src/Description.php
@@ -0,0 +1,54 @@
+<?php namespace Ctrl\Discourse\Api;
+
+use GuzzleHttp\Command\Guzzle\Description as BaseDescription;
+
+class Description extends BaseDescription
+{
+    /** @var array */
+    protected $defaults = [
+        'operations' => [
+            'groups' => [
+                'httpMethod' => 'GET',
+                'uri' => '/admin/groups.json',
+                'responseModel' => 'jsonResponse'
+            ],
+            'categories' => [
+                'httpMethod' => 'GET',
+                'uri' => '/categories.json',
+                'responseModel' => 'jsonResponse'
+            ],
+            'categoryLatestTopics' => [
+                'httpMethod' => 'GET',
+                'uri' => '/c/{slug}/l/latest.json',
+                'responseModel' => 'jsonResponse',
+                'parameters' => [
+                    'slug' => [
+                        'type' => 'string',
+                        'location' => 'uri'
+                    ]
+                ]
+            ]
+        ],
+        'models' => [
+            'jsonResponse' => [
+                'type' => 'object',
+                'additionalProperties' => [
+                    'location' => 'json'
+                ]
+            ]
+        ]
+    ];
+
+    /**
+     * Description Constructor.
+     *
+     * @param array $config
+     * @param array $options
+     */
+    public function __construct(array $config = [], array $options = [])
+    {
+        $config = array_merge_recursive($this->defaults, $config);
+
+        parent::__construct($config, $options);
+    }
+}
